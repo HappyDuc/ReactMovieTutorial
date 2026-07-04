043e4304ac97e84907ae15403a554924e12a59f7
Jake Sinclair <=>
2026-07-04 18:08:21 +0100

    Added env to gitignore and also made that change redundant by being stupid but its ok :)

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = "";
+const API_KEY = "82991fcc4b2bf4ab76b2d1a2d0e4cb25"; // Yes this is wrong, yes I am doing it anyway.
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
